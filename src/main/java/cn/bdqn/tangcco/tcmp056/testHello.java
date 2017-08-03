c46ed1cef3a78a46df0b40cf3b55079cc2644331
mrwuyunfeng <913821317@qq.com>
2017-08-03 18:35:41 +0800

    hello world

---
+++ src/main/java/cn/bdqn/tangcco/tcmp056/testHello.java
@@ -0,0 +1,15 @@
+package cn.bdqn.tangcco.tcmp056;
+
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2017/8/3 0003.
+ */
+public class testHello {
+    @Test
+    public void sayHello() {
+        System.out.println("hello");
+        System.out.println("world");
+    }
+
+}
---
+++ src/main/java/cn/bdqn/tangcco/tcmp056/testWorld.java
@@ -0,0 +1,14 @@
+package cn.bdqn.tangcco.tcmp056;
+
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2017/8/3 0003.
+ */
+public class testWorld {
+
+    @Test
+    public void sayHello() {
+        System.out.println("hello");
+    }
+}
